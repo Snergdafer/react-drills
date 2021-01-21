9e2fc57940cde2a78d9bf9f39bcd7985812e9133
Jesse Etherington <jesse.etherington@gmail.com>
2021-01-21 16:48:23 -0700

    Did app-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      colors: ["blue", "orange", "green", "red", "maroon", "black"],
+      userInput: ''
+    }
+  }
+
+  updateInput(input) {
+    this.setState({userInput: input})
+  }
+
+  render() {
+    let displayArray = this.state.colors
+      .filter((elem) => {
+        return elem.includes(this.state.userInput)
+      })
+      .map((elem, i) => {
+        return <h2 key={i}>{elem}</h2>
+      })
+
+    return (
+      <div className="App">
+        <input onChange={e => this.updateInput(e.target.value)} type="text"/>
+        {displayArray}
+      </div>
+    );
+  }
 }
 
 export default App;
