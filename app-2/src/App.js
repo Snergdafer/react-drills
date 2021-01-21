e4d2082539c178448b8c694af9949a69a42b2742
Jesse Etherington <jesse.etherington@gmail.com>
2021-01-21 15:31:41 -0700

    Removed unused files from app-1 and finished app-2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      arr: ["Little Timmy ", "BJ Helmsworth ", "Arnor Shmortsineggerr ", "Regeneld Furgunsburgur "]
 
+    }
+  }
+
+
+  
+
+
+
+  render() {
+    let listo = this.state.arr.map((elem, i) => {
+
+      return <span key={i}>{elem}</span>
+
+    })
+
+    return (
+       <div className="App">
+         {listo}
+      </div>
+    );
+  }
+}
 export default App;
